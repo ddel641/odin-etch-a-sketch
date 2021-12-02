f949b44bacb17814dcb57ab51905d4f696414794
ddel641 <daniel.delpiano@gmail.com>
2021-12-02 14:40:00 -0500

    Add button to clear sketch and set new grid

--- code.js
+++ code.js
@@ -1,40 +1,56 @@
-let numberOfBoxes = 16;
 const sketchArea = document.querySelector(".sketchArea");
 
 
-// create the etch-a-sketch grid
-for (let i = 0; i < numberOfBoxes; i++ ){
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < numberOfBoxes; j++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        row.appendChild(box);
+// Create the etch-a-sketch grid
+function initialize(numberOfBoxes) {
+    // Clear the sketch area
+    while (sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild);
     }
-    sketchArea.appendChild(row);
 
+    // Draw the sketch area
+    for (let i = 0; i < numberOfBoxes; i++ ){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < numberOfBoxes; j++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            row.appendChild(box);
+        }
+        sketchArea.appendChild(row);
+    }
+
+    boxListener(); // add listeners to the grid
 }
 
+
+// Resets the drawing area. allows a new number of boxes
 function clearDrawing(){
-    let filled = document.querySelectorAll(".drawn");
-    filled.forEach((box) => {
-        box.classList.remove("drawn");
-    });
+    let num;
+    let keepRunning = true;
+    while(keepRunning) {
+        num = parseInt(prompt("Choose a number of boxes between 10 and 100?"));
+        keepRunning = (isNaN(num) || num > 100 || num < 10)
+    }
+    
+    initialize(num);
+
 }
 
 
+// Create an event listener that changes box bg-color when mouseover
+function boxListener() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
 
-//Button event listener to remove grid bg-color
+        box.addEventListener("mouseenter", () => {
+            box.classList.add("drawn");
+        });
+    })
+}
+
+initialize(16);
+
+// Button event listener to reset grid
 const btn = document.querySelector("#update");
 btn.addEventListener("click", clearDrawing);
-
-    
-    
-// Create an event listener that changes box bg-color when mouseover
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => {
-
-    box.addEventListener("mouseenter", () => {
-        box.classList.add("drawn");
-    });
-})
