5cd5114f85a2dc7d6edc5f3859336d1751d95995
ddel641 <daniel.delpiano@gmail.com>
2021-12-01 23:08:00 -0500

    Add code and styles to change bg on mouseover

--- code.js
+++ code.js
@@ -1,20 +1,40 @@
-let boxes = 16;
+let numberOfBoxes = 16;
 const sketchArea = document.querySelector(".sketchArea");
-console.log(sketchArea);
-function addRows() {
-    for (let i = 0; i < boxes; i++ ){
-        let row = document.createElement("div");
-        row.classList.add("row");
-        for (let j = 0; j < boxes; j++) {
-            let box = document.createElement("div");
-            box.classList.add("box");
-            row.appendChild(box);
-        }
-        sketchArea.appendChild(row);
 
+
+// create the etch-a-sketch grid
+for (let i = 0; i < numberOfBoxes; i++ ){
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < numberOfBoxes; j++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        row.appendChild(box);
     }
+    sketchArea.appendChild(row);
+
 }
 
-const btn = document.querySelector("#update");
+function clearDrawing(){
+    let filled = document.querySelectorAll(".drawn");
+    filled.forEach((box) => {
+        box.classList.remove("drawn");
+    });
+}
 
-btn.addEventListener("click", addRows);
+
+
+//Button event listener to remove grid bg-color
+const btn = document.querySelector("#update");
+btn.addEventListener("click", clearDrawing);
+
+    
+    
+// Create an event listener that changes box bg-color when mouseover
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+
+    box.addEventListener("mouseenter", () => {
+        box.classList.add("drawn");
+    });
+})
